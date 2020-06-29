85434fdcb06ca111ae0d782a99b1bc136c61187d
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2020-06-29 15:08:46 +0800

    Generate skaffold cljs serverless project

---
+++ basicsls/README.md
@@ -0,0 +1,25 @@
+# basicsls
+
+# Install Dependencies
+
+```shell
+$ lein deps
+```
+
+# Deploy
+
+```shell
+$ serverless deploy
+```
+
+# Redeploy Function
+
+```
+$ serverless deploy function -f echo
+```
+
+# Invoke
+
+```shell
+$ curl -X POST <url> -H 'Content-Type: application/json' -d '{"body": "Hi"}'
+```
---
+++ basicsls/project.clj
@@ -0,0 +1,14 @@
+(defproject basicsls "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure       "1.9.0"]
+                 [org.clojure/clojurescript "1.10.312"]
+                 [io.nervous/cljs-lambda    "0.3.5"]]
+  :plugins [[lein-npm                    "0.6.2"]
+            [io.nervous/lein-cljs-lambda "0.6.6"]]
+  :npm {:dependencies [[serverless-cljs-plugin "0.1.2"]]}
+  :cljs-lambda {:compiler
+                {:inputs  ["src"]
+                 :options {:output-to     "target/basicsls/basicsls.js"
+                           :output-dir    "target/basicsls"
+                           :target        :nodejs
+                           :language-in   :ecmascript5
+                           :optimizations :simple}}})
